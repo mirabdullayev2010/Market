7f056416d3baa19fbf54b71d0d68d4769b0ce0c9
Zikirillo11 <zikrilloyoldashev11@gmail.com>
2026-04-10 16:55:44 +0500

    modellar togirlandi

--- Market.Domain/Entites/Order.cs
+++ Market.Domain/Entites/Order.cs
@@ -1,9 +1,18 @@
-﻿namespace Market.Domain.Entites;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Market.Domain.Entites;
+
+[Table("Orders")]
 public class Order : BaseEntity
 {
+    [Column("TotalAmount")]
     public decimal TotalAmount { get; set; }
+
+    [Column("UserId")]
     public Guid UserId { get; set; }
+
+    [Column("Users")]
     public User User { get; set; } = null!;
     public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
 }
--- Market.Domain/Entites/Product.cs
+++ Market.Domain/Entites/Product.cs
@@ -1,11 +1,25 @@
-﻿namespace Market.Domain.Entites;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Market.Domain.Entites;
+
+[Table("Products")]
 public class Product : BaseEntity
 {
+    [Column("name")]
     public string Name { get; set; } = null!;
+
+    [Column("Decription")]
     public string? Description { get; set; }
+
+    [Column("Price")]
     public decimal Price { get; set; }
+
+    [Column("StockQuantity")]
     public int StockQuantity { get; set; }
+
+    [Column("CategoryId")]
     public Guid CategoryId { get; set; }
+
+    [Column("Category")]
     public Category Category { get; set; } = null!;
 }
